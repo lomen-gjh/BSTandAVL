2caf2dfc7fee84437edfef79493954f250a78930
David Lomen <lomen@gjh.sk>
2025-04-14 13:11:52 +0200

    insert function in GUI

--- GUI.py
+++ GUI.py
@@ -3,13 +3,17 @@ import BST
 window=tk.Tk()
 window.title("Binary Search Tree and AVL Tree Visualization")
 canvas=tk.Canvas(window, width=800, height=600, bg="white")
-button=tk.Button(window, text="Insert", command=lambda: None) # Placeholder for insert function
+button=tk.Button(window, text="Insert", command=lambda: insert(int(entry.get()))) # Placeholder for insert function
 entry=tk.Entry(window)
 canvas.pack(side='right')
 entry.pack()
 button.pack()
 
 bst=BST.BinarySearchTree() #novy strom
+
+def insert(value):
+    bst.insert(value) #insert value into the BST
+    bst.drawTree(400, 20, canvas) #draw the BST
 
 #function to insert a value into the BST and draw it
 
